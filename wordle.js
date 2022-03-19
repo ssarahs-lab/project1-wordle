3aa850664247ae168f2c82847546f0d4d3ebe0c0
Sarah S <mrs.sarahso@gmail.com>
2022-03-19 17:59:57 +1100

    using includes on compareWords

--- wordle.js
+++ wordle.js
@@ -1,84 +1,65 @@
+let userInput = document.getElementById("userInput");
+
  //Computer choose a random word from word list
  //Wishlist for later: computer keep same random word for 24 hours depending on user's geo-location
 
- function computerChoose(){
+function computerChoose(){
 
-    let randomIndex = Math.floor(Math.random()*validWords.length);
-    let computerChoice = validWords[randomIndex];
+    let randomIndex = Math.floor(Math.random()*testWords.length);
+    let computerChoice = testWords[randomIndex];
 
-    
-
+    console.log(computerChoice);
     return computerChoice;
 
  }
 
+const testWords = ['AALII', 'AARGH', 'AARTI', 'ABACA']
+
  //list for guessing, human words for those answers
- 
-let userInput = document.getElementById("userInput").value;
-
-console.log(computerChoose());
-
-let computerChoiceArray = computerChoose().split("");
-let userInputArray = userInput.split("");
-
-console.log(computerChoiceArray);
-console.log(userInputArray);
 
 function compareWords(){
     
    // Get input from user
- //turn both inputs into an array
- //
+   //turn both inputs into an array
 
+   let userInputValue = userInput.value; 
+   let userInputArray = userInputValue.split("");
+   let userInputArrayUpperCase = userInputArray.toUpperCase();
+   let computerChoiceArray = computerChoose().split("");
+
+   //if letter and index match, then green
+   //if letter only matches, then yellow
+
+   console.log(userInputArray)
+
+   if(userInputArray.includes(computerChoiceArray) == true ){
+
+      console.log()
+
+   }
+  
+  //make green class, if 5 classlength green - "you've won"
+   //make guess counter, end game at 6 guesses
+   
+  
+  
+  
+  
+   // for (const letter of computerChoiceArray){
+
+      //    for(index in userInputArray){
  
+      //       if(userInputArray[index] == letter){
 
+      //          console.log(userInputArray[index]);
+      //       }
 
-//computer compare index text
+      //    }
+         
+      // }
+
+   //computer compare index text
+
 }
 
  
-
- // check if matches 
-
-//count number of guesses
-
-
-    //   let figures = document.getElementById("userInput").value
-    //   let placeHolder = ".";
-
-    //   for (const word of words){
-
-    //         if (word.match(figures, placeHolder)){
-
-    //             const element = document.createElement('p');
-    //             element.textContent = word;
-    //             document.body.appendChild(element);
-
-    //         } else if (word.match(figures, placeHolder, placeHolder)){
-
-    //             const element = document.createElement('p');
-    //             element.textContent = word;
-    //             document.body.appendChild(element);
-
-    //         } else if (word.match(figures,placeHolder,placeHolder, placeHolder)){
-
-    //             const element = document.createElement('p');
-    //             element.textContent = word;
-    //             document.body.appendChild(element);
-            
-    //         } else if (word.match(figures,placeHolder,placeHolder, placeHolder, placeHolder)){
-
-    //             const element = document.createElement('p');
-    //             element.textContent = word;
-    //             document.body.appendChild(element);
-        
-    //         }
-    //     }
-
-    //     //clears user input bar after search is done
-
-    //     userInput = document.getElementById("userInput");
-    //     userInput.value ="";
-
-
-    
